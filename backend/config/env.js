dec49cd65d4a5a950789a7d6c003b8f3e0bcce0e
Arman <mdarmanjes@gmail.com>
2026-01-24 15:13:58 +0600

    added centralized env var

---
+++ backend/config/env.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const requiredENV = [
+  "PORT",
+  "MONGODB_COMPASS_URI",
+  "ACCESS_TOKEN_SECRET",
+  "ACCESS_TOKEN_EXPIREY",
+  "NODE_ENV",
+];
+
+for (const key of requiredENV) {
+  if (!process.env[key]) {
+    throw new Error(`❌ Missing environment variable: ${key}`);
+  }
+}
+
+export const env = {
+  PORT: process.env.PORT,
+  MONGODB_COMPASS_URI: process.env.MONGODB_COMPASS_URI,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIREY: process.env.ACCESS_TOKEN_EXPIREY,
+  NODE_ENV: process.env.NODE_ENV || "development",
+};
