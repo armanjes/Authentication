7413c2ea13b45942979fff3b8cbaab3cb561e09b
Arman <mdarmanjes@gmail.com>
2026-02-07 12:42:14 +0600

    refactor env vairables in config

--- backend/config/env.js
+++ backend/config/env.js
@@ -15,10 +15,12 @@ for (const key of requiredENV) {
   }
 }
 
-export const env = {
+const env = {
   PORT: process.env.PORT,
   MONGODB_COMPASS_URI: process.env.MONGODB_COMPASS_URI,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   ACCESS_TOKEN_EXPIREY: process.env.ACCESS_TOKEN_EXPIREY,
   NODE_ENV: process.env.NODE_ENV || "development",
 };
+
+export default env;
