fcd2ce6d363ed5b21f46acf4191c71116db5fd94
Arman <mdarmanjes@gmail.com>
2026-04-07 10:44:06 +0600

    AUTH STEP COMMENTS

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,28 +6,108 @@ import userModel from "../models/user.model.js";
  * @route POST /api/auth/register
  * @access Public
  */
-export const register = asyncHandler(async (req, res) => {});
+export const register = asyncHandler(async (req, res) => {
+  /**
+   * validate name, email, password
+   * check if user exists with email
+   * create user
+   * generate token
+   * send response
+   */
+
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existingUser = await userModel.findOne({ email });
+
+  if (existingUser) {
+    throw new ApiError(409, "Email already exists.");
+  }
+
+  const user = new userModel({ name, email, password });
+
+  const accessToken = user.accessToken();
+
+  await user.save();
+
+  return res
+    .status(201)
+    .cookie("accessToken", accessToken)
+    .json(new ApiResponse(user, "User created successfull"));
+});
 
 /**
  * @desc Login user
  * @route POST /api/auth/login
  * @access Public
  */
-export const login = asyncHandler(async (req, res) => {});
+export const login = asyncHandler(async (req, res) => {
+  /**
+   * validate email & password
+   * find user with email
+   * compare password
+   * generate token
+   * send response
+   */
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    throw new ApiError(401, "Invalid credentials.");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Invalid credentials.");
+  }
+
+  const accessToken = user.accessToken();
+
+  const data = {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+    __v: user.__v,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken)
+    .json(new ApiResponse(data, "Login successfull"));
+});
 
 /**
  * @desc Get current logged-in user
  * @route POST /api/auth/me
  * @access Private
  */
-export const getCurrentUser = (req, res) => {};
+export const getCurrentUser = (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(req.user, "User fetching successfull."));
+};
 
 /**
  * @desc Logout user
  * @route POST /api/auth/logout
  * @access Private
  */
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  return res
+    .status(200)
+    .clearCookie("accessToken")
+    .json(new ApiResponse({}, "Logout successful."));
+};
 
 /**
  * @desc Update profile
